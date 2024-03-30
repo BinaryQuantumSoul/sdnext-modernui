3d456d0290d43c7efb25f803e46e5ebbd225b922
QuantumSoul <jeuxcooletgratuit.simdif@gmail.com>
2024-03-30 21:28:41 +0100

    Small improvements
    - Center AR
    - Reformat settings

--- scripts/sdnext_uiux.py
+++ scripts/sdnext_uiux.py
@@ -1,28 +1,29 @@
 # Originally from Anapnoe@https://github.com/anapnoe/stable-diffusion-webui-ux/blob/8307896c59032a9cdac1ab24c975102ff9a674d3/extensions-builtin/anapnoe-sd-uiux/scripts/anapnoe_sd_uiux.py
 
 import gradio as gr
-from modules import script_callbacks, shared
+from modules import script_callbacks
+from modules.shared import OptionInfo, options_section , options_templates
 
 def on_ui_settings():
-    shared.options_templates.update(shared.options_section(('uiux_core', "UI-UX"), {
-        'uiux_separator_appearance': shared.OptionInfo("<h2>Appearance</h2>", "", gr.HTML),
-        "uiux_no_slider_layout": shared.OptionInfo(False, "Hide input range sliders"),
-        "uiux_show_input_range_ticks": shared.OptionInfo(True, "Show ticks for input range slider"),
-        "uiux_show_labels_aside": shared.OptionInfo(False, "Show labels for aside tabs"),
-        "uiux_show_labels_main": shared.OptionInfo(False, "Show labels for main tabs"),
-        "uiux_show_labels_tabs": shared.OptionInfo(False, "Show labels for page tabs"),
-        "uiux_show_labels_control": shared.OptionInfo(False, "Show labels for control tabs"),
-        "uiux_no_headers_params": shared.OptionInfo(False, "Hide params headers"),
-        "uiux_show_outline_params": shared.OptionInfo(True, "Show parameter outline"),
+    options_templates.update(options_section(('uiux_core', "UI-UX"), {
+        'uiux_separator_appearance': OptionInfo("<h2>Appearance</h2>", "", gr.HTML),
+        "uiux_no_slider_layout": OptionInfo(False, "Hide input range sliders"),
+        "uiux_show_input_range_ticks": OptionInfo(True, "Show ticks for input range slider"),
+        "uiux_show_labels_aside": OptionInfo(False, "Show labels for aside tabs"),
+        "uiux_show_labels_main": OptionInfo(False, "Show labels for main tabs"),
+        "uiux_show_labels_tabs": OptionInfo(False, "Show labels for page tabs"),
+        "uiux_show_labels_control": OptionInfo(False, "Show labels for control tabs"),
+        "uiux_no_headers_params": OptionInfo(False, "Hide params headers"),
+        "uiux_show_outline_params": OptionInfo(True, "Show parameter outline"),
 
-        'uiux_separator_mobile': shared.OptionInfo("<h2>Mobile</h2>", "", gr.HTML),
-        "uiux_default_layout": shared.OptionInfo("Auto", "Layout", gr.Radio, {"choices": ["Auto","Desktop", "Mobile"]}),  
-        "uiux_mobile_scale": shared.OptionInfo(0.7, "Mobile scale", gr.Slider, {"minimum": 0.5, "maximum": 1, "step": 0.05}),
+        'uiux_separator_mobile': OptionInfo("<h2>Mobile</h2>", "", gr.HTML),
+        "uiux_default_layout": OptionInfo("Auto", "Layout", gr.Radio, {"choices": ["Auto","Desktop", "Mobile"]}),  
+        "uiux_mobile_scale": OptionInfo(0.7, "Mobile scale", gr.Slider, {"minimum": 0.5, "maximum": 1, "step": 0.05}),
 
-        'uiux_separator_other': shared.OptionInfo("<h2>Other Settings</h2>", "", gr.HTML),
-        "uiux_enable_console_log": shared.OptionInfo(True, "Enable debug log"),
-        "uiux_max_resolution_output": shared.OptionInfo(2048, "Max resolution output for txt2img and img2img"),
-        "uiux_disable_transitions": shared.OptionInfo(False, "Disable transitions")
+        'uiux_separator_other': OptionInfo("<h2>Other Settings</h2>", "", gr.HTML),
+        "uiux_enable_console_log": OptionInfo(True, "Enable debug log"),
+        "uiux_max_resolution_output": OptionInfo(2048, "Max resolution output for txt2img and img2img"),
+        "uiux_disable_transitions": OptionInfo(False, "Disable transitions")
     }))
 
 script_callbacks.on_ui_settings(on_ui_settings)
