9d584a1bdc0c2aca614aa0e1e34e4374c3aa779d
vladmandic <mandic00@live.com>
2026-03-24 10:06:37 +0100

    disable legacy tabs by default
    Signed-off-by: vladmandic <mandic00@live.com>

--- scripts/sdnext_modernui.py
+++ scripts/sdnext_modernui.py
@@ -8,7 +8,7 @@ def on_ui_settings():
         'uiux_separator_appearance': OptionInfo("<h2>Appearance</h2>", "", gr.HTML),
         "uiux_grid_image_size": OptionInfo(150, "Grid image size", gr.Slider, {"minimum": 64, "maximum": 1024, "step": 1}),
         "uiux_panel_min_width": OptionInfo(35, "Panel minimum width", gr.Number),
-        "uiux_hide_legacy": OptionInfo(False, "Hide legacy tabs"),
+        "uiux_hide_legacy": OptionInfo(True, "Hide legacy tabs"),
         "uiux_persist_layout": OptionInfo(True, "Persist UI layout"),
         "uiux_no_slider_layout": OptionInfo(False, "Hide input range sliders"),
         "uiux_show_labels_aside": OptionInfo(False, "Show labels for aside tabs"),
